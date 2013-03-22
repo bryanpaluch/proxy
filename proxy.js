7a969bc774f2837e745ae06dae4dee75975841fc
Ubuntu <ubuntu@ip-10-110-202-11.ec2.internal>
2013-03-22 14:31:56 +0000

    first commit

---
+++ proxy.js
@@ -0,0 +1,21 @@
+var httpProxy = require('http-proxy');
+
+
+var options = {
+  router: {
+    'rtcwith.me'                  : 'localhost:3000',
+    'commwith.me'                 : 'localhost:3000',
+    'bryanpaluch.com'             : 'localhost:3001',
+    'www.bryanpaluch.com'         : 'localhost:3001',
+    'homemonitor.bryanpaluch.com' : 'localhost:3003',
+    'connector.gridreact.com'     : 'localhost:3010',
+    'hooks.gridreact.com'         : 'localhost:3011',
+    'hooks.bryanpaluch.com'       : 'localhost:3011',
+    'up.bryanpaluch.com'          : 'localhost:2812',
+    'gridreact.com'               : 'localhost:3003',
+    'interact.gridreact.com'      : 'localhost:3003'
+  }
+}
+
+httpProxy.createServer(options).listen(80);
+console.log('proxy listening on 80');
